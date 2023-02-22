7eb9b768bed27775f53f6dd4a6785ca560ed4ad2
sew347 <sew347@psu.edu>
2023-02-22 08:04:37 +0800

    start partway subspaces

--- subspace_recovery.py
+++ subspace_recovery.py
@@ -5,7 +5,7 @@ import single_subspace_recovery as ssr
 import scipy.io as sio
 
 
-def recover_subspaces(Y, s, output_folder, J=None, cov_proj = True):
+def recover_subspaces(Y, s, output_folder, start=0, J=None, cov_proj = True):
     """
     Recovers bases for the spanning subspaces from sample matrix Y.
     :param Y: Data matrix with samples as columns
@@ -18,20 +18,22 @@ def recover_subspaces(Y, s, output_folder, J=None, cov_proj = True):
         J = np.shape(Y)[1]
     N = np.shape(Y)[1]
     cov = (Y @ np.conjugate(Y).T)/N
-    corrs = Y[:,:J].T @ np.conjugate(Y)
-    for i in range(J):
+    corrs = Y[:,:start+J].T @ np.conjugate(Y)
+    for i in range(start, start+J):
+        print(i)
         subspace_curr = ssr.recover_subspace(i, s, Y, cov, corrs = corrs[i,:], cov_proj = cov_proj)
         filename = output_folder + '/subspace_' + str(i) +'.npy'
         np.save(filename, subspace_curr)
         if np.mod(i+1, 50) == 0:
             print(f'Computed {i+1} subspaces successfully.')
 
-def recover_subspaces_from_file(sample_file, s, output_folder, J = None, cov_proj = True, matlab_name = 'Y'):
+def recover_subspaces_from_file(sample_file, s, output_folder, start=0, J=None, cov_proj=True, matlab_name='Y'):
     """
     Wrapper for recover_subspaces to be called from file for samples.
     :param sample_file: location of file in .npy or .mat format containing matrix with samples as columns
     :param s: Expected sparsity
     :param output_folder: If set, subspaces will be saved in this location
+    :param start: Starting index for recovery. If not set, starts at 0.
     :param J: Number of subspaces to recover. If not set, all will be recovered.
     :param cov_proj: Whether to use covariance projection step.
     :param matlab_name: Variable name of matlab element to load as Y matrix. Defaults to 'Y'.
@@ -47,7 +49,7 @@ def recover_subspaces_from_file(sample_file, s, output_folder, J = None, cov_pro
         Y = mat_dict[matlab_name]
     else:
         raise ValueError('File extension for sample_file must be either .npy or .mat.')
-    return recover_subspaces(Y, s, output_folder, J = J, cov_proj = cov_proj)
+    return recover_subspaces(Y, s, output_folder, start=start, J = J, cov_proj = cov_proj)
 
 
 if __name__ == "__main__":
@@ -58,6 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('--s', type=int, help='Expected sparsity', required=True)
     parser.add_argument('--output_folder', help='Folder for saving output', required = True)
     parser.add_argument('--J', type=int, help='Number of subspaces to compute', required = False)
+    parser.add_argument('--start', type=int, help='Starting index', required = False, default=0)
     parser.add_argument('--no_cov_proj', help='Flag to exclude covariance projection step', action='store_true', required = False)
     parser.add_argument('--matlab_name', type=str, help='Variable name for .mat files', default = 'Y', required = False)
     args = parser.parse_args()
@@ -71,6 +74,7 @@ if __name__ == "__main__":
         args.s,
         args.output_folder,
         J = args.J,
+        start = args.start,
         cov_proj = not(args.no_cov_proj),
         matlab_name = args.matlab_name,
     )
